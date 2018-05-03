daa8c1c6b779c687e0832d30155fe8f376842b79
Alok Kulkarni <kulkarni.alok@gmail.com>
2018-05-03 21:57:14 +0100

    ngrx store added to tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
 
+import { StoreModule } from '@ngrx/store'
+import { reducer } from './reducers/tutorial.reducers'
+
 import { environment } from '../environments/environment';
+import { ReadComponent } from './read/read.component';
+import { CreateComponent } from './create/create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReadComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
+    StoreModule.forRoot({tutorial: reducer})
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
